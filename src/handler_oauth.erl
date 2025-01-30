6ea10f3b83c51e118d082ca4af86fc28e4a4f402
chiroptical <chiroptical@gmail.com>
2025-01-29 19:33:39 -0500

    Add query parameter to index to indicate completion

--- src/handler_oauth.erl
+++ src/handler_oauth.erl
@@ -4,43 +4,40 @@
     init/2
 ]).
 
-%% TODO: Need a handler for Twitch's response /oauth/end
-%% TODO: Store credentials in mnesia
+index(~"") ->
+    <<"<!doctype html>", "<html>", "<head>", "<title>Authorize with Twitch</title>", "</head>",
+        "<body>", "<form action=\"/oauth/begin\" method=\"get\">",
+        "<input type=\"submit\" value=\"Begin\">", "</form>", "</body>", "</html>">>;
+index(Msg) ->
+    <<"<!doctype html>", "<html>", "<head>", "<title>Authorize with Twitch</title>", "</head>",
+        "<body>", "<p>", Msg/binary, "</p>", "</body>", "</html>">>.
+
 init(#{path := ~"/", method := ~"GET"} = Req0, State) ->
     %% TODO: Add button to POST /oauth/begin
+    #{message := Message} = cowboy_req:match_qs([{message, [], ~""}], Req0),
     Req = cowboy_req:reply(
         200,
         #{~"content-type" => ~"text/html"},
-        ~"""
-        <!doctype html>
-        <html>
-          <head>
-            <title>Authorize with Twitch</title>
-          </head>
-          <body>
-            <form action="/oauth/begin" method="get">
-              <input type="submit" value="Begin">
-            </form>
-          </body>
-        </html>
-        """,
+        index(Message),
         Req0
     ),
     {ok, Req, State};
 init(#{path := ~"/oauth/begin", method := ~"GET"} = Req0, State) ->
-    %% TODO: Redirect to initiate Twitch oauth
+    %% TODO: Redirect to initiate Twitch OAuth
     Req = cowboy_req:reply(
         303,
-        #{~"location" => ~"/"},
+        #{~"location" => ~"/oauth/end"},
         ~"",
         Req0
     ),
     {ok, Req, State};
+%% TODO: Finish this handler for Twitch's GET
+%% TODO: Store the results in an mnesia table
 init(#{path := ~"/oauth/end", method := ~"GET"} = Req0, State) ->
     %% TODO: Handle Twitch request and store information
     Req = cowboy_req:reply(
         303,
-        #{~"location" => ~"/"},
+        #{~"location" => ~"/?message=All done"},
         ~"",
         Req0
     ),
