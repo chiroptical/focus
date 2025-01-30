e9fc4e334e51540e605fe5298f5c01ebf9aa8738
chiroptical <chiroptical@gmail.com>
2025-01-29 22:14:19 -0500

    Get some documentation and TODOs in place

--- src/handler_oauth.erl
+++ src/handler_oauth.erl
@@ -4,6 +4,7 @@
     init/2
 ]).
 
+% TODO: Use https://github.com/bunopnu/e2h to generate views
 index(~"") ->
     <<"<!doctype html>", "<html>", "<head>", "<title>Authorize with Twitch</title>", "</head>",
         "<body>", "<form action=\"/oauth/begin\" method=\"get\">",
@@ -12,8 +13,8 @@ index(Msg) ->
     <<"<!doctype html>", "<html>", "<head>", "<title>Authorize with Twitch</title>", "</head>",
         "<body>", "<p>", Msg/binary, "</p>", "</body>", "</html>">>.
 
+%% TODO: We don't need to redirect to /oauth/begin first
 init(#{path := ~"/", method := ~"GET"} = Req0, State) ->
-    %% TODO: Add button to POST /oauth/begin
     #{message := Message} = cowboy_req:match_qs([{message, [], ~""}], Req0),
     Req = cowboy_req:reply(
         200,
@@ -22,8 +23,19 @@ init(#{path := ~"/", method := ~"GET"} = Req0, State) ->
         Req0
     ),
     {ok, Req, State};
+%% TODO: Redirect to initiate Twitch OAuth
+%% DOCS: https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#authorization-code-grant-flow
 init(#{path := ~"/oauth/begin", method := ~"GET"} = Req0, State) ->
-    %% TODO: Redirect to initiate Twitch OAuth
+    _TwitchUrl = restc:construct_url(
+        "https://id.twitch.tv/oauth2/authorize",
+        [
+            {"client_id", "..."},
+            {"redirect_uri", "..."},
+            {"response_type", "code"},
+            {"scope", "..."},
+            {"state", "..."}
+        ]
+    ),
     Req = cowboy_req:reply(
         303,
         #{~"location" => ~"/oauth/end"},
@@ -31,10 +43,10 @@ init(#{path := ~"/oauth/begin", method := ~"GET"} = Req0, State) ->
         Req0
     ),
     {ok, Req, State};
-%% TODO: Finish this handler for Twitch's GET
+%% TODO: Finish this handler for Twitch's redirect, see 'DOCS' above
 %% TODO: Store the results in an mnesia table
+%% TODO: Use the authorization code to get a token, see 'DOCS' above
 init(#{path := ~"/oauth/end", method := ~"GET"} = Req0, State) ->
-    %% TODO: Handle Twitch request and store information
     Req = cowboy_req:reply(
         303,
         #{~"location" => ~"/?message=All done"},
