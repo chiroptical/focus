4d5d4397b004c04cecbf4de2e9c97c2ae27cb5c3
Barry Moore <chiroptical@gmail.com>
2025-02-05 08:48:57 -0500

    Start mnesia setup and create/read

--- src/server_twitch_credentials.erl
+++ src/server_twitch_credentials.erl
@@ -4,6 +4,11 @@
 
 -export([
     start_link/2,
+    setup_mnesia/0,
+    create/4,
+    read/1
+]).
+-export([
     init/1,
     handle_call/3,
     handle_cast/2,
@@ -16,26 +21,75 @@
 %       Should happen as continue in init/1 and in check_credentials loop.
 % TODO: Add a function refresh_credentials/0
 
-start_link(ClientId, Secret) ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, {ClientId, Secret}, []).
-
--record(state, {
+-record(twitch_credentials, {
     client_id,
     secret,
     access_token = none,
     refresh_token = none
 }).
 
+setup_mnesia() ->
+    Nodes = [node()],
+    case mnesia:create_schema(Nodes) of
+        ok ->
+            logger:notice("Created mnesia schema"),
+            ok;
+        {error, {_, {already_exists, _}}} ->
+            logger:notice("Schema already exists");
+        {error, Err} ->
+            logger:notice(#{create_schema => failed_with, error => Err})
+    end,
+    application:start(mnesia),
+    case
+        mnesia:create_table(
+            twitch_credentials,
+            [
+                {attributes, record_info(fields, twitch_credentials)},
+                {disc_copies, Nodes}
+            ]
+        )
+    of
+        {atomic, ok} ->
+            ok;
+        {aborted, {already_exists, _}} ->
+            logger:notice(#{twitch_credentials_table => already_exists}),
+            ok
+    end,
+    application:stop(mnesia).
+
+create(ClientId, Secret, AccessToken, RefreshToken) ->
+    Txn = fun() ->
+        mnesia:write(
+            #twitch_credentials{
+                client_id = ClientId,
+                secret = Secret,
+                access_token = AccessToken,
+                refresh_token = RefreshToken
+            }
+        )
+    end,
+    mnesia:activity(transaction, Txn).
+
+% TODO: Returns [{twitch_credentials, ClientId, Secret, AccessToken, Refresh}]
+read(ClientId) ->
+    Txn = fun() ->
+        mnesia:read(twitch_credentials, ClientId)
+    end,
+    mnesia:activity(transaction, Txn).
+
+start_link(ClientId, Secret) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, {ClientId, Secret}, []).
+
 -define(THIRTY_MINUTES_MS, 1_800_000).
 
 init({ClientId, Secret}) ->
     erlang:send_after(?THIRTY_MINUTES_MS, self(), check_credentials),
-    {ok, #state{client_id = ClientId, secret = Secret}}.
+    {ok, #twitch_credentials{client_id = ClientId, secret = Secret}}.
 
 handle_call(
     read,
     _From,
-    #state{access_token = AccessToken, refresh_token = RefreshToken} = State
+    #twitch_credentials{access_token = AccessToken, refresh_token = RefreshToken} = State
 ) ->
     case {AccessToken, RefreshToken} of
         {none, _} -> {reply, no_credentials, State};
@@ -44,13 +98,13 @@ handle_call(
     end.
 
 handle_cast({update, AccessToken, RefreshToken}, _State) ->
-    {noreply, #state{access_token = AccessToken, refresh_token = RefreshToken}};
+    {noreply, #twitch_credentials{access_token = AccessToken, refresh_token = RefreshToken}};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
 handle_info(
     check_credentials,
-    #state{access_token = AccessToken, refresh_token = RefreshToken} = State
+    #twitch_credentials{access_token = AccessToken, refresh_token = RefreshToken} = State
 ) ->
     erlang:send_after(?THIRTY_MINUTES_MS, self(), check_credentials),
 
